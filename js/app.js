d545bec6aedd991731b560597d76fd66b20d5be6
baradweelrawan <bardaweel_rawan@yahoo.com>
2021-04-25 16:15:45 +0300

    done

---
+++ js/app.js
@@ -0,0 +1,13 @@
+'user strict'
+alert ('WELCOME')
+    let username=  prompt('plaes Enter What kind Of Computer You Need(PC,laptop)');
+
+    console.log(username);
+    let Name=  prompt('Pleas Enter Your Name');
+    console.log('The Name is'+ Name );
+    let price= parseInt( prompt('Pleas Enter the price that you want'));
+    console.log('the price i want is '+ price );
+
+
+
+
